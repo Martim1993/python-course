a06aa1a493e5ac94bea123be79e43b0767602c70
Martim1993 <timirbulatovmarsel@gmail.com>
2025-01-30 19:32:02 +0300

    dz 1.2

---
+++ dz_1.9.py
@@ -0,0 +1,24 @@
+input_list = input('Вход: ')
+input_list = list(input_list)
+
+if input_list == []:
+    print('Ваш список пустой. Введите пожалуйста список чисел')
+
+else:
+    def find_max_recursive(lst):
+
+        if len(lst) == 1:
+            return lst[0]
+
+        max_element = find_max_recursive(lst[1:])
+
+        if lst[0] > max_element:
+            return lst[0]
+
+        else:
+            return max_element
+
+
+    print(f'Выход: {find_max_recursive(input_list)}')
+
+
